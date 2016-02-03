2386bce12293b420074088ac73118f6d789b3a1b
ornette coleman <omniosi@gmail.com>
2016-02-03 14:45:06 -0800

    "WelcomeToTheShow" and "PickUpTheSticks"

---
+++ mlb-com/MLBTS16_PickUpTheSticksBlue_LearnMore_728x90_National_v1_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_PickUpTheSticksBlue_LearnMore_728x90_National_v1.html",
+	"width":728,
+	"height":90,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
---
+++ mlb-com/MLBTS16_WelcomeToTheShowBlue_LearnMore_300x250_National_v1_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_WelcomeToTheShowBlue_LearnMore_300x250_National_v1.html",
+	"width":300,
+	"height":250,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
