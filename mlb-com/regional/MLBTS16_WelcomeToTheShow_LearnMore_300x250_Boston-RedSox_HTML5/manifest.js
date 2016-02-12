f414e08cb57f1f9aeb351bef98213f58d7e947da
ornette coleman <omniosi@gmail.com>
2016-02-12 15:59:05 -0800

    3rd banner done

--- mlb-com/regional/MLBTS16_WelcomeToTheShow_LearnMore_300x250_Boston-RedSox_HTML5/manifest.js
+++ mlb-com/regional/MLBTS16_WelcomeToTheShow_LearnMore_300x250_Boston-RedSox_HTML5/manifest.js
@@ -1,5 +1,5 @@
 FT.manifest({
-	"filename": "MLBTS16_TheShowIsComing_LearnMore_300x250_Boston-RedSox.html",
+	"filename": "MLBTS16_WelcomeToTheShow_LearnMore_300x250_Boston-RedSox.html",
 	"width":300,
 	"height":250,
 	"hideBrowsers":["ie8","opera"],
