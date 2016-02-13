51fa099d08d5e0333c27959d33fd29a0f060caa0
ornette coleman <omniosi@gmail.com>
2016-02-12 16:30:19 -0800

    2 more Boston banners

---
+++ mlb-com/regional/MLBTS16_PutOnYour-AndDance_LearnMore_728x90_Boston_Red-Sox_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_PutOnYour-AndDance_LearnMore_728x90_Boston_Red-Sox.html",
+	"width":728,
+	"height":90,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
---
+++ mlb-com/regional/MLBTS16_PutOnYour-AndDance_LearnMore_970x66_Boston_Red-Sox_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_PutOnYour-AndDance_LearnMore_970x66_Boston_Red-Sox.html",
+	"width":728,
+	"height":90,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
