22973f8024b24b4f9346a934c93ba789d86e601c
ornette coleman <omniosi@gmail.com>
2016-02-15 02:08:24 -0800

    KC Royals done.

---
+++ mlb-com/regional/kc-royals/MLBTS16_AllHailTheKing_LearnMore_728x90_KC-Royals_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_AllHailTheKing_LearnMore_728x90_KC-Royals.html",
+	"width":728,
+	"height":90,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
---
+++ mlb-com/regional/kc-royals/MLBTS16_AllHailTheKing_LearnMore_970x66_KC-Royals_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_AllHailTheKing_LearnMore_970x66_KC-Royals.html",
+	"width":970,
+	"height":66,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
