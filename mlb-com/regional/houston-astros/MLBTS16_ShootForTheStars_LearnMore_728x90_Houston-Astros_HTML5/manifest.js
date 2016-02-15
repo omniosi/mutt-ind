58663345990943b1d6a0d1703e1d768fd798a306
ornette coleman <omniosi@gmail.com>
2016-02-15 00:04:15 -0800

    Houston Astros Done.

---
+++ mlb-com/regional/houston-astros/MLBTS16_ShootForTheStars_LearnMore_728x90_Houston-Astros_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_ShootForTheStars_LearnMore_728x90_Houston-Astros.html",
+	"width":728,
+	"height":90,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
---
+++ mlb-com/regional/houston-astros/MLBTS16_ShootForTheStars_LearnMore_970x66_Houston-Astros_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_ShootForTheStars_LearnMore_970x66_Houston-Astros.html",
+	"width":970,
+	"height":66,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
