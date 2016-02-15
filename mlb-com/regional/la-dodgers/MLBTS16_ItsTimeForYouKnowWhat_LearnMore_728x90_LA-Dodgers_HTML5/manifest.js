a77aa7b48d602a6e22cc9043d42d4919a148f71e
ornette coleman <omniosi@gmail.com>
2016-02-15 14:26:21 -0800

    LA Dodgers done.

---
+++ mlb-com/regional/la-dodgers/MLBTS16_ItsTimeForYouKnowWhat_LearnMore_728x90_LA-Dodgers_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_ItsTimeForYouKnowWhat_LearnMore_728x90_LA-Dodgers.html",
+	"width":728,
+	"height":90,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
---
+++ mlb-com/regional/la-dodgers/MLBTS16_ItsTimeForYouKnowWhat_LearnMore_970x66_LA-Dodgers_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_ItsTimeForYouKnowWhat_LearnMore_970x66_LA-Dodgers.html",
+	"width":970,
+	"height":66,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
