2ead2fc2690c64615eda55db2dfdccda7ac57caa
ornette coleman <omniosi@gmail.com>
2016-02-15 16:10:45 -0800

    NY Yankees

---
+++ mlb-com/regional/ny-yankees/MLBTS16_PutOnYourPinstripes_LearnMore_970x66_NY-Yankees_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_PutOnYourPinstripes_LearnMore_970x66_NY-Yankees.html",
+	"width":970,
+	"height":66,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
---
+++ mlb-com/regional/ny-yankees/MLBTS16_WelcomeToTheShow_LearnMore_300x250_NY-Yankees_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_WelcomeToTheShow_LearnMore_300x250_NY-Yankees.html",
+	"width":300,
+	"height":250,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
