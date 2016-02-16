79113772c88e28fa130614bea35765448d03b5f8
ornette coleman <omniosi@gmail.com>
2016-02-15 23:10:42 -0800

    st.louis cardinals done

---
+++ mlb-com/regional/stlouis-cardinals/MLBTS16_RiseUpRedbirdNation_LearnMore_728x90_StLouis-Cardinals_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_RiseUpRedbirdNation_LearnMore_728x90_StLouis-Cardinals.html",
+	"width":728,
+	"height":90,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
---
+++ mlb-com/regional/stlouis-cardinals/MLBTS16_RiseUpRedbirdNation_LearnMore_970x66_StLouis-Cardinals_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_RiseUpRedbirdNation_LearnMore_970x66_StLouis-Cardinals.html",
+	"width":970,
+	"height":66,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
