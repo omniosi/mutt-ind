ca0e5972808664e61efd8d664e40627096dfa5a9
ornette coleman <omniosi@gmail.com>
2016-02-16 01:15:19 -0800

    Toronto Blue Jays done

---
+++ mlb-com/regional/toronto-bluejays/MLBTS16_GetYourHat-GetYourBat_LearnMore_728x90_Toronto-BlueJays_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_GetYourHat-GetYourBat_LearnMore_728x90_Toronto-BlueJays.html",
+	"width":728,
+	"height":90,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
---
+++ mlb-com/regional/toronto-bluejays/MLBTS16_GetYourHat-GetYourBat_LearnMore_970x66_Toronto-BlueJays_HTML5/manifest.js
@@ -0,0 +1,7 @@
+FT.manifest({
+	"filename": "MLBTS16_GetYourHat-GetYourBat_LearnMore_970x66_Toronto-BlueJays.html",
+	"width":970,
+	"height":66,
+	"hideBrowsers":["ie8","opera"],
+	"clickTagCount":1
+});
